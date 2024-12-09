b62d18a5f89f0fe30ee99eac3b96c8f4bf75d0fa
rahma <sherifrahma829@gmail.com>
2024-12-09 10:34:10 +0200

    create user info list tile

--- lib/widgets/desktop_layout.dart
+++ lib/widgets/desktop_layout.dart
@@ -1,3 +1,4 @@
+import 'package:dash_board/widgets/custom_drawer.dart';
 import 'package:flutter/material.dart';
 
 class DesktopLayout extends StatelessWidget {
@@ -5,6 +6,10 @@ class DesktopLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Row(
+      children: [
+        Expanded(child: CustomDrawer()),
+      ],
+    );
   }
 }
---
+++ lib/widgets/user_info_list_tile.dart
@@ -0,0 +1,32 @@
+import 'package:dash_board/utils/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class UserInfoListTile extends StatelessWidget {
+  const UserInfoListTile(
+      {super.key,
+      required this.image,
+      required this.title,
+      required this.subtitle});
+
+  final String image, title, subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: const Color(0xffFAFAFA),
+      elevation: 0,
+      child: ListTile(
+        leading: SvgPicture.asset(image),
+        title: Text(
+          title,
+          style: Styles.styleSemiBold16(context),
+        ),
+        subtitle: Text(
+          subtitle,
+          style: Styles.styleRegular12(context),
+        ),
+      ),
+    );
+  }
+}
