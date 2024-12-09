0079b26a64a9696157c6242d031d50eed419716e
rahma <sherifrahma829@gmail.com>
2024-12-09 11:58:16 +0200

    finish custom drawer

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,4 +1,7 @@
+import 'package:dash_board/models/drawer_item_model.dart';
 import 'package:dash_board/utils/app_images.dart';
+import 'package:dash_board/widgets/active_and_inactive_item.dart';
+import 'package:dash_board/widgets/drawer_items_list_view.dart';
 import 'package:dash_board/widgets/user_info_list_tile.dart';
 import 'package:flutter/material.dart';
 
@@ -9,12 +12,39 @@ class CustomDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: const Column(
-        children: [
-          UserInfoListTile(
-              image: Assets.imagesAvatar3,
-              title: 'Rahma Sherif',
-              subtitle: 'rahma@gmail.com'),
+      child: const CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: UserInfoListTile(
+                image: Assets.imagesAvatar3,
+                title: 'Rahma Sherif',
+                subtitle: 'rahma@gmail.com'),
+          ),
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 8,
+            ),
+          ),
+          DrawerItemsListView(),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                Expanded(child: SizedBox()),
+                InActiveDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                      title: 'Setting system', image: Assets.imagesSettings),
+                ),
+                InActiveDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                      title: 'Logout account', image: Assets.imagesLogout),
+                ),
+                SizedBox(
+                  height: 48,
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
---
+++ lib/widgets/drawer_item.dart
@@ -0,0 +1,18 @@
+import 'package:dash_board/models/drawer_item_model.dart';
+import 'package:dash_board/widgets/active_and_inactive_item.dart';
+import 'package:flutter/material.dart';
+
+class DrawerItem extends StatelessWidget {
+  const DrawerItem(
+      {super.key, required this.drawerItemModel, required this.isActive});
+
+  final DrawerItemModel drawerItemModel;
+  final bool isActive;
+  @override
+  Widget build(BuildContext context) {
+    return isActive
+        ? ActiveDrawerItem(drawerItemModel: drawerItemModel)
+        : InActiveDrawerItem(drawerItemModel: drawerItemModel);
+  }
+}
+
