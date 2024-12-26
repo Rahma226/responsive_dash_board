2fd81d8928d0f3b8234b4d3118fc955d3b5e9ef4
rahma <sherifrahma829@gmail.com>
2024-12-26 13:12:35 +0200

    my cards section done

--- lib/widgets/desktop_layout.dart
+++ lib/widgets/desktop_layout.dart
@@ -1,6 +1,8 @@
-import 'package:dash_board/widgets/all_expenses.dart';
 import 'package:dash_board/widgets/custom_drawer.dart';
-import 'package:dash_board/widgets/quick_invoice.dart';
+import 'package:dash_board/widgets/my_card.dart';
+import 'package:dash_board/widgets/my_card_section.dart';
+import 'package:dash_board/widgets/my_cards_page_view.dart';
+import 'package:dash_board/widgets/section2.dart';
 import 'package:flutter/material.dart';
 
 class DesktopLayout extends StatelessWidget {
@@ -16,23 +18,14 @@ class DesktopLayout extends StatelessWidget {
         ),
         Expanded(
           flex: 3,
-          child: Column(
-            children: [
-              SizedBox(
-                height: 40,
-              ),
-              AllExpenses(),
-              SizedBox(
-                height: 24,
-              ),
-              QuickInvoice(),
-            ],
-          ),
+          child: section2(),
         ),
         SizedBox(
           width: 24,
         ),
-        Expanded(flex: 2, child: Placeholder()),
+        Expanded(
+          child: MyCardSection(),
+        ),
       ],
     );
   }
---
+++ lib/widgets/my_card.dart
@@ -0,0 +1,71 @@
+import 'package:dash_board/utils/app_images.dart';
+import 'package:dash_board/utils/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class MyCard extends StatelessWidget {
+  const MyCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 420 / 215,
+      child: Container(
+        decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          image: const DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(Assets.imagesCardBackground),
+          ),
+          color: const Color(0xff4EB7F2),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              contentPadding:
+                  const EdgeInsets.only(left: 31, right: 42, top: 16),
+              title: Text(
+                'Name Card',
+                style: Styles.styleRegular16(context).copyWith(
+                  color: Colors.white,
+                ),
+              ),
+              subtitle: Text(
+                'Rahma Sherif',
+                style: Styles.styleMedium20(context),
+              ),
+              trailing: SvgPicture.asset(Assets.imagesGallery),
+            ),
+            const Expanded(child: SizedBox()),
+            Padding(
+              padding: const EdgeInsets.only(right: 48 - 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    '8483 4663 0443 1256',
+                    style: Styles.styleSemiBold24(context).copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    '12/2 - 124',
+                    style: Styles.styleRegular16(context).copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 54 - 28,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
